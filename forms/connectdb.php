cb0052d19394e8426cccaa2df167b480d42a02cc
shelton15 <sheltoniyassa@gmail.com>
2023-06-08 17:20:42 +0100

    database added

---
+++ forms/connectdb.php
@@ -0,0 +1,15 @@
+#how to connect database to website using php?
+<?php
+// <!--DELIMITER-->Example configuration:
+$databaseHost = 'localhost';
+$databaseUsername = 'Mondocta_database';
+$databasePassword = '';
+$databaseName = 'Mondocta_database';
+
+// <!--DELIMITER-->Run the application using the built-in web server.
+
+
+// <!--DELIMITER-->Source: https://github.com/mariadb-developers/php-quickstart
+
+?>
+
